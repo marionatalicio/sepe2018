2d154122cade8114de4b79a790cbf0e2c7808205
ArthurDCastro <arthurcastro078@gmail.com>
2018-09-04 20:14:46 -0300

    a

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Calculator 3000</title>
+    <link rel="stylesheet" type="text/css" href="assets/vendor/semantic/semantic.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendor/semantic/components/header.css">
+    <script src="assets/vendor/jquery/jquery.js"></script>
+    <script src="assets/vendor/semantic/semantic.js"></script>
+    <script src="assets/front/index.js"></script>
+</head>
+
+<div class="ui container" style="margin: 20px 0">
+    <h2 class="ui center aligned icon header">
+        <i class="users icon"></i>
+        Calculator 3000
+    </h2>
+</div>
+<div class="ui container segment">
+    <form class="ui form">
+        <div class="field">
+            <label>Country</label>
+            <select class="ui search dropdown">
+                <option value="">Select Country</option>
+                <option value="a">a</option>
+            </select>
+        </div>
+        <div class="ui two column grid">
+            <div class="column">
+                <h2 class="ui header">
+                    <i class="mars icon"></i>
+                    <div class="content">
+                        Pai
+                    </div>
+                </h2>
+                <div class="field">
+                    <input type="text">
+                </div>
+            </div>
+            <div class="column">
+                <h2 class="ui header">
+                    <i class="venus icon"></i>
+                    <div class="content">
+                        Mãe
+                    </div>
+                </h2>
+            </div>
+        </div>
+    </form>
+</div>
+
+</html>
